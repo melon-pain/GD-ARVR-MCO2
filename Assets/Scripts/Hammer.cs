4705a059691aa72b1c74f1c3d1fe2750868002d9
EM-Neon <blindsuneclipse@gmail.com>
2022-06-20 10:02:02 +0800

    finished with hammer

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class Hammer : MonoBehaviour
 {
+    public ObserverBehaviour hammerTarget;
+    private bool isTracked = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,33 +16,65 @@ public class Hammer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTracked)
+        {
+            Ray r = Camera.main.ScreenPointToRay(RayCaster());
+            RaycastHit hit;
+
+            if (Physics.Raycast(r, out hit, 100, LayerMask.GetMask("Default")))
+            {
+                if(hit.collider.TryGetComponent<Spawner>(out Spawner spawner))
+                {
+
+                    if (!spawner.isSpawned)
+                        return;
+
+                    switch (spawner.currentType)
+                    {
+                        case SpawnType.Mole:
+                            GameManager.instance.PlaySound(3);
+                            GameManager.instance.AddScore();
+                            break;
+                        case SpawnType.Trap:
+                            GameManager.instance.RemoveAllLives();
+                            break;
+                        case SpawnType.Impostor:
+                            GameManager.instance.PlaySound(1);
+                            GameManager.instance.AddBonusScore();
+                            break;
+                    }
+
+                    spawner.Despawn();
+                    Debug.Log("Bonk");
+                }
+                Debug.Log(hit.collider.name);
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTargetStatusChanged(ObserverBehaviour target, TargetStatus targetStatus)
     {
-        if (other.TryGetComponent<Spawner>(out Spawner spawner))
+        if (targetStatus.Status == Status.NO_POSE)
         {
-            if (!spawner.isSpawned)
-                return;
-
-            switch (spawner.currentType)
-            {
-                case SpawnType.Mole:
-                    GameManager.instance.PlaySound(3);
-                    GameManager.instance.AddScore();
-                    break;
-                case SpawnType.Trap:
-                    GameManager.instance.RemoveAllLives();
-                    break;
-                case SpawnType.Impostor:
-                    GameManager.instance.PlaySound(1);
-                    GameManager.instance.AddBonusScore();
-                    break;
-            }
-
-            spawner.Despawn();
-            Debug.Log("Bonk");
+            OnTargetLost();
         }
+        else
+        {
+            OnTargetDetected();
+        }
+    }
+    public void OnTargetDetected()
+    {
+        isTracked = true;
+    }
+
+    public void OnTargetLost()
+    {
+        isTracked = false;
+    }
+
+    private Vector2 RayCaster()
+    {
+        return Camera.main.WorldToScreenPoint(hammerTarget.transform.position);
     }
 }
